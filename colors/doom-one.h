1ef8ae2c90cb96b7356c5fdf2c40c9fcd07fd415
Ivan Mišustin <ivanmis596@gmail.com>
2022-05-27 20:18:28 +0200

    updated the colors to be blue

--- colors/doom-one.h
+++ colors/doom-one.h
@@ -1,9 +1,9 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#cccccc", "#282c34" },
-	[SchemeSel] = { "#1c1f24", "#c678dd" },
-	[SchemeSelHighlight] = { "#c678dd", "#1c1f24" },
-	[SchemeNormHighlight] = { "#c678dd", "#1c1f24" },
+	[SchemeSel] = { "#1c1f24", "#5e81ac" },
+	[SchemeSelHighlight] = { "#5e81ac", "#1c1f24" },
+	[SchemeNormHighlight] = { "#5e81ac", "#1c1f24" },
 	[SchemeOut] = { "#000000", "#51afef" },
 	[SchemeMid] = { "#d7d7d7", "#1c1f24" },
 };
