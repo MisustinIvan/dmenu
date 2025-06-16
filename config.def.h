b6e2546884fe78751b154398d04cf26e566c3022
MisustinIvan <ivanmis596@gmail.com>
2025-06-16 17:14:50 +0200

    update cfg

--- config.def.h
+++ config.def.h
@@ -4,21 +4,32 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=14"
+    "Iosevka Nerd Font Mono:size=13",
+	"monospace:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-/*static const char *colors[SchemeLast][2] = { */
-	/*     fg         bg       */
-/*  [SchemeNorm] = { "#f8ecaa", "#344e5b" },
-	[SchemeSel] = { "#344e5b", "#e3433f" },
-	[SchemeOut] = { "#344e5b", "#344e5b" },
-}; */
+
+// colorscheme
+#define col_gray1 "#222222"
+#define col_gray2 "#444444"
+#define col_gray3 "#bbbbbb"
+#define col_gray4 "#eeeeee"
+#define col_cyan "#344e5b"
+#define col_green "#6ac769"
+#define col_yellow "#e5c63f"
+#define col_white "#f8ecaa"
+#define col_red "#e3433f"
+
+#define col_1 "#cf8592"
+#define col_2 "#165a5d"
+#define col_3 "#eab8bb"
+#define col_4 "#298657"
 
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-    [SchemeNorm] = { "#e0def4", "#232136" },
-	[SchemeSel] = { "#232136", "#c4a7e7" },
-	[SchemeOut] = { "#232136", "#232136" },
+    [SchemeNorm] = { col_3, col_2 },
+	[SchemeSel] = { col_2,  col_1},
+	[SchemeOut] = { col_2, col_2 },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
